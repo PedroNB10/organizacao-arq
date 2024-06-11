15092d891604ea0ddb3e23ea1f412bd6a6e6e132
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-06-11 14:29:53 -0300

    adiciona comentários.

--- aula0606/ex01.asm
+++ aula0606/ex01.asm
@@ -4,8 +4,8 @@
 # soma += v[i]
 
 .data
-
-v: .word 1,2,3,4,5
+# vetor de 5 posições
+v: .word 1,2,3,4,5 # nessa linha estamos alocando 5 palavras de 4 bytes cada, ou seja, 20 bytes
 
 
 .text
@@ -13,6 +13,7 @@ v: .word 1,2,3,4,5
 .globl main
 
 main:
+    # li = load immediate
     li $s0, 0 # soma = 0
     li $s1, 0 # i = 0
     li $s3, 5 # n = 5 tamanho do vetor
@@ -25,16 +26,22 @@ inicio:
     # deslocamento de duas casas e multiplicação por 4
     # indice 0
     # t1 armazena o deslocamento
+    # sll = shift left logical
+    # t1 armazena o deslocamento
+    # s1 é o i que será multiplicado por 4 (deslocamento de 2 casas binárias para a esquerda)
     sll $t1, $s1, 2 # 4 * i, nesse caso não precisa de shift left, pois o deslocamento é 0, mas por didática vamos fazer
     # deslocamento de duas casas do binário para a esquerda é a mesma coisa que multiplicar por 4
     # endereço 0: base + deslocamento
 
-    # endereço 0:
-    add $s2, $t0, $t1 # s2 = base + deslocamento      
+    # endereço i:
 
-    # somando valor
+    # o endereço é armazenado em s2. s2 é o endereço de memória do elemento v[i] atual
+    add $s2, $t0, $t1 # deslocando o endereço base para o endereço do elemento v[i] atual
+
+    # carregando o valor no endereço de memória de s2 para t2
     lw $t2, 0($s2) # t2 = v[i], 0 é o deslocamento, precisa do 0 porque o lw precisa desse deslocamento para não referenciar o label mas sim o endereço de memória
 
+    # soma = soma + v[i]
     add $s0, $s0, $t2 # soma = soma + v[i]
 
     # incrementar o i 
@@ -42,3 +49,8 @@ inicio:
     j inicio
 
 fim:
+
+    # imprime o resultado
+    add $a0, $zero, $s0 # passando o valor de s0 para o argumento da syscall
+    li $v0, 1 # código da syscall para imprimir inteiro
+    syscall
